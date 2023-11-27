1a31a133ee5ee74612896c5e8fbc46e49dee628f
1aphla2 <ahsanbukhariahsanshan@gmail.com>
2023-11-27 23:41:21 +0500

    my files

---
+++ java.js
@@ -0,0 +1,24 @@
+// for ( let r =0;r<5; r++ )
+// console.log(`r`,`what is your `)
+
+// console.log(`mw name is ahsan`)
+// console.log(`mw name is ahsan`)
+// console.log(`mw name is ahsan`)
+// console.log(`mw name is ahsan`)
+// console.log(`mw name is ahsan`)
+alert(`only for important personalities`)
+prompt(`what is your name`)
+
+let nr1 = 10;
+let nr2 = 2;
+let result = nr1**nr2;
+console.log(result)
+
+let nr3 = 15;
+let nr4 = 3;
+let nr5 = 10;
+let nr6= 3;
+let obtained = nr3 % nr4 ;
+console.log(`${nr3} % ${nr4} = ${obtained}`)
+let outcome = nr5 % nr6 ;
+console.log(outcome)
